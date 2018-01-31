58fc5577d30163b6dc7aeb796249c734db34b9a7
Yining Wang <biyao323@gmail.com>
2018-01-30 17:02:52 -0800

    last lab

--- exercise-wk2/exercise1.R
+++ exercise-wk2/exercise1.R
@@ -7,9 +7,9 @@ my.vector <- c(my.num, initials)
 
 # run ?sum to get more info
 vector.sum <- sum(my.vector)
-
+?sum
 # Describe why this doesn't work: 
-# 
+# Sum takes only integer values
 
 install.packages("stringr")
 
@@ -18,12 +18,11 @@ my.line <- "Hey, hey, this is the library"
 print(str_length(my.line))
 
 # Describe why this doesn't work: 
-# 
+# str_length is not a function
 
 said.the.famous <- paste(my.line, " - ", initial)
-
 # Describe why this doesn't work: 
-# 
+# object 'initial' not found
 
 
 ## Part 2 - Vector and function practice
@@ -32,10 +31,12 @@ said.the.famous <- paste(my.line, " - ", initial)
 
 # Write a function `CompareLength` that takes in 2 vectors, and returns the sentence:
 # "The difference in lengths is N"
-
-
+CompareLength <- function(first, second) {
+  return (paste("The difference in lengths is ", nchar(first) - nchar(second)))
+}
+ 
 # Pass two vectors of different length to your `CompareLength` function
-
+CompareLength("aaa", "cc")
 
 # Write a function `DescribeDifference` that will return one of the following statements:
 # "Your first vector is longer by N elements"
